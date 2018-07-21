9e4915896cb8ab026013d8a1659f422bc3b6ca69
Emily Jirles <emily.jirles@gmail.com>
2018-07-21 17:13:02 +0000

    automatically backed up by learn

--- app/serializers/products_serializer.rb
+++ app/serializers/products_serializer.rb
@@ -1,6 +1,11 @@
 class ProductSerializer
   def serialize(product)
     serialized_data = '{'
-    serialized_data += '' 
+    serialized_data += '"name": "' + product.name + '", '
+    serialized_data += '"price"'
+    serialized_data += '"inventory"'
+    serialized_data += '"description"'
+    serialized_data += '}'
+    serialized_data
   end
 end
