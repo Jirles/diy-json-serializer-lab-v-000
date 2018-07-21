029c04c50d498a01291247a69f243dab9978b26c
Emily Jirles <emily.jirles@gmail.com>
2018-07-21 17:14:11 +0000

    automatically backed up by learn

--- app/serializers/products_serializer.rb
+++ app/serializers/products_serializer.rb
@@ -4,7 +4,7 @@ class ProductSerializer
     serialized_data += '"name": "' + product.name + '", '
     serialized_data += '"price": "' + product.price + '", '
     serialized_data += '"inventory": "' + product.inventory + '", '
-    serialized_data += '"description"'
+    serialized_data += '"description": "' + product.description + '"'
     serialized_data += '}'
     serialized_data
   end
