817ee28854b66c9dcdd4768c5b7547bfa611eaa6
Emily Jirles <emily.jirles@gmail.com>
2018-07-21 17:23:56 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
-  resources :products, only: [:index, :new, :create]
+  resources :products, only: [:index, :new, :create, :show]
   get '/products/:id/data', to: 'products#data'
 end
